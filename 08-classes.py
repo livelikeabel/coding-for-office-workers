f89c4be604c553b9bc449da0105bf169ddb98f35
abelko <esung1129@gmail.com>
2017-11-09 17:49:59 +0900

    pleaseeeeeeee make commit log

--- 08-classes.py
+++ 08-classes.py
@@ -29,7 +29,26 @@ article2 = Article2("개발", "개발은 쉬워요")
 article3 = Article2("코칭", "코칭은 쉬워요")
 article4 = Article2("요리", "요리는 쉬워요")
 
+"""
 print(article2.title, article2.content)
 print(article2.view_count)
 article2.read()
 print(article2.view_count)
+"""
+
+
+
+
+#### Article class inheritance 상속
+class BrunchArticle(Article2):
+    source = "브런치"
+
+    def read(self):
+        self.view_count = self.view_count + 2
+
+brunch_article = BrunchArticle("개발", "개발은 쉬워요")
+print(brunch_article.title)
+print(brunch_article.content)
+print(brunch_article.view_count)
+brunch_article.read()
+print(brunch_article.view_count)
